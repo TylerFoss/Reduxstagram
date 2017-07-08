0e784ca99655310e6c5ea03cda0049ef7167b536
Tyler Foss <tylerscottfoss@gmail.com>
2017-07-07 21:10:54 -0600

    routes added

---
+++ client/components/Single.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import { Link } from 'react-router';
+
+class Single extends Component{
+  render(){
+    return(
+      <div className="single-photo">
+        Im the single Photo
+      </div>
+    )
+  }
+}
+
+export default Single;
