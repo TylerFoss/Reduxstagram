bd884a4fa7d0b5caa7a91c6b832c7618f3fc4ffe
Tyler Foss <tylerscottfoss@gmail.com>
2017-07-12 21:37:11 -0600

    Photogrid populated reducers and action completed

---
+++ client/actions/actionCreator.js
@@ -0,0 +1,30 @@
+// increment a like
+// an action is an object with information and whats supposed to happen
+// every time you dispatch an action every reducer fires
+// increment
+export function increment(index) {
+  return {
+    type: 'INCREMENT_LIKES',
+    index
+  }
+}
+
+// add comment
+export function addComment(postId, author, comment) {
+  return {
+    type: 'ADD_COMMENT',
+    postId,
+    author,
+    comment
+  }
+}
+
+// remove comment
+
+export function removeComment(postId, i) {
+  return {
+    type: 'REMOVE_COMMENT',
+    i,
+    postId
+  }
+}
