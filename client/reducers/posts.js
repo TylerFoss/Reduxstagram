b712f8bf7c965e6ff9c2226b75282457aa08792d
Tyler Foss <tylerscottfoss@gmail.com>
2017-07-14 21:40:44 -0600

    increment reducer linked

--- client/reducers/posts.js
+++ client/reducers/posts.js
@@ -5,7 +5,18 @@
 
 
 function posts(state = [], action){
-  console.log(state, action);
-  return state;
+  switch(action.type){
+    case "INCREMENT_LIKES" :
+    // return the update state
+    console.log("increment likes")
+    const i = action.index;
+    return [
+       ...state.slice(0,i), //before the one were updating
+       {...state[i], likes: state[i].likes + 1},
+       ...state.slice(i + 1), //after the one we are updating
+    ]
+    default:
+      return state;
+  }
 }
 export default posts;
