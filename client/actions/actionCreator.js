aaca8fdc61b3a65bb7e980b74130a751199f6a68
Tyler Foss <tylerscottfoss@gmail.com>
2017-07-17 17:48:02 -0600

    comments added

--- client/actions/actionCreator.js
+++ client/actions/actionCreator.js
@@ -11,6 +11,7 @@ export function increment(index) {
 
 // add comment
 export function addComment(postId, author, comment) {
+  console.log("this is dispatched")
   return {
     type: 'ADD_COMMENT',
     postId,
